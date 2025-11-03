a6192b024b881ed8ae6e53da17ae1cf2e5d907b4
hebx <hebamaher0101@gmail.com>
2025-11-03 21:16:04 +0200

    Create intiate_database.sql

---
+++ scripts/intiate_database.sql
@@ -0,0 +1,25 @@
+use master;
+
+
+-- Drop and recreate the 'DataWarehouse' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
+
+create DATABASE DataWarehouse;
+GO
+
+use DataWarehouse;
+GO
+
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
